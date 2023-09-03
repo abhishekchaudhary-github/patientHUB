2a9188fb6ce88de97af238aec4238a7b4804e228
abhishekchaudhary-github <abhishekch.165@gmail.com>
2023-09-03 13:19:30 +0530

    test update

--- src/test/java/com/project/project/helper/PatientHelper.java
+++ src/test/java/com/project/project/helper/PatientHelper.java
@@ -1,2 +1,14 @@
-package com.project.project.helper;public class PatientHelper {
+package com.project.project.helper;
+import com.project.project.entity.Patient;
+import java.time.LocalDate;
+
+public class PatientHelper {
+    public static Patient makePatient(Integer id, String name, LocalDate dob, String address) {
+        Patient patient = new Patient();
+        patient.setId(id);
+        patient.setName(name);
+        patient.setDob(dob);
+        patient.setAddress(address);
+        return patient;
+    }
 }
--- src/test/java/com/project/project/service/PatientServiceTest.java
+++ src/test/java/com/project/project/service/PatientServiceTest.java
@@ -1,23 +1,24 @@
 package com.project.project.service;
 
 import com.project.project.entity.Patient;
+import com.project.project.helper.PatientHelper;
 import com.project.project.repository.PatientRepository;
-import com.project.project.service.PatientService;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.junit.jupiter.api.Test;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 import org.assertj.core.api.Assertions;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 public class PatientServiceTest {
@@ -29,12 +30,8 @@ public class PatientServiceTest {
     @Test
     public void testCreatePatient() {
         // Create a sample patient
-        Patient samplePatient = new Patient();
-        samplePatient.setId(1);
-        samplePatient.setName("John");
-        samplePatient.setDob(LocalDate.of(1985, 5, 15));
-        samplePatient.setAddress("xyz");
-        when(patientRepository.save(Mockito.any(Patient.class))).thenReturn(samplePatient);
+        Patient samplePatient = PatientHelper.makePatient(1,"John",LocalDate.of(1985, 5, 15),"xyz");
+        when(patientRepository.save(any(Patient.class))).thenReturn(samplePatient);
         Patient savedPatient = patientService.createPatient(samplePatient);
         Assertions.assertThat(savedPatient).isNotNull();
     }
@@ -43,28 +40,36 @@ public class PatientServiceTest {
     public void testGetAllPatients() {
         // Create a list of sample patients
         List<Patient> samplePatients = new ArrayList<>();
-        Patient samplePatient = new Patient();
-        samplePatient.setId(1);
-        samplePatient.setName("John");
-        samplePatient.setDob(LocalDate.of(1985, 5, 15));
-        samplePatient.setAddress("xyz");
-        Patient samplePatient2 = new Patient();
-        samplePatient2.setId(1);
-        samplePatient2.setName("John");
-        samplePatient2.setDob(LocalDate.of(1985, 5, 15));
-        samplePatient2.setAddress("xyz");
-
+        Patient samplePatient = PatientHelper.makePatient(1,"John",LocalDate.of(1985, 5, 15),"xyz");
+        Patient samplePatient2 = PatientHelper.makePatient(1,"John",LocalDate.of(1988, 3, 12),"123");
         samplePatients.add(samplePatient);
         samplePatients.add(samplePatient2);
-
         when(patientRepository.findAll()).thenReturn(samplePatients);
-
         List<Patient> patients = patientService.getAllPatients();
+        assertNotNull(patients);
+        assertEquals(2, patients.size());
+    }
 
-        org.junit.jupiter.api.Assertions.assertNotNull(patients);
-        org.junit.jupiter.api.Assertions.assertEquals(2, patients.size());
-
+    @Test
+    public void testUpdatePatient() {
+        Patient samplePatient = PatientHelper.makePatient(1,"John",LocalDate.of(1985, 5, 15),"xyz");
+        when(patientRepository.findById(1)).thenReturn(Optional.of(samplePatient));
+        when(patientRepository.save(any(Patient.class))).thenReturn(samplePatient);
+        Patient updatedPatient = PatientHelper.makePatient(1,"Smart",LocalDate.of(1985, 5, 15),"xyz");
+        Patient response = patientService.updatePatient(1,updatedPatient);
+        assertNotNull(response);
+        assertEquals(updatedPatient.getName(), "Smart");
 
     }
+
+    @Test
+    public void testDeletePatient() {
+        Patient samplePatient = PatientHelper.makePatient(1,"John",LocalDate.of(1985, 5, 15),"xyz");
+        when(patientRepository.findById(1)).thenReturn(Optional.ofNullable(samplePatient));
+        doNothing().when(patientRepository).delete(samplePatient);
+        assertAll(() -> patientService.deletePatient(1));
+    }
+
+
 
     }
